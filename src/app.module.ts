62414d5f5589e24bedd4c7b59457a2d8673182fb
tzhangm <tzhangm@trip.com>
2022-10-20 14:08:01 +0800

    fix: add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './apps/test/test.module';
+import {CoreModule} from "./apps/core/core.module";
 
 @Module({
-  imports: [TestModule],
+  imports: [TestModule,CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/apps/core/controller/workspace.controller.ts
@@ -0,0 +1,20 @@
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    UseGuards,
+    Request,
+} from "@nestjs/common";
+import { ListWorkspaceService } from "../service/list-workspace.service";
+
+@Controller()
+export class WorkspaceController {
+    constructor(
+        private listWorkspaceService: ListWorkspaceService
+    ) {}
+    @Post("listworkspace")
+    listworkspace(@Body() reqBody):Promise<any> {
+        return this.listWorkspaceService.invoke(reqBody);
+    }
+}
